c6b8751849339668dd02d01be4a96c3f16149793
Evan Azevedo <me@evanazevedo.com>
2024-02-21 16:39:52 -0700

    Applying keymaps

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,10 @@
 -- define common options
 local opts = {
-	noremap = true, -- non-recursive
-	silent = true, -- do not show message
+    noremap = true, -- non-recursive
+    silent = true, -- do not show message
 }
 
--- vim.g.mapleader = " "
+vim.g.mapleader = " "
 
 -----------------
 -- Normal mode --
@@ -12,7 +12,31 @@ local opts = {
 
 -- Hint: see `:h vim.map.set()`
 -- Better window navigation
-vim.keymap.set("n", "<Leader-h>", "<C-w>h", opts)
-vim.keymap.set("n", "<Leader-j>", "<C-w>j", opts)
-vim.keymap.set("n", "<Leader-k>", "<C-w>k", opts)
-vim.keymap.set("n", "<Leader-l>", "<C-w>l", opts)
+vim.keymap.set("n", "<Leader>h", "<C-w>h", opts)
+vim.keymap.set("n", "<Leader>j", "<C-w>j", opts)
+vim.keymap.set("n", "<Leader>k", "<C-w>k", opts)
+vim.keymap.set("n", "<Leader>l", "<C-w>l", opts)
+
+-- Move visual lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep cursor in center of screen when using U and D
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Keep search terms in the middle
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Keep yanked text when pasting over
+vim.keymap.set("x", "<leader>p", '\"_dP')
+
+-- Leader y to yank unto system clipboard
+vim.keymap.set("n", "<leader>y", '\"+y')
+vim.keymap.set("v", "<leader>y", '\"+y')
+vim.keymap.set("n", "<leader>Y", '\"+Y')
+
+-- Delete to null register with leader d
+vim.keymap.set("n", "<leader>d", '\"_d')
+vim.keymap.set("v", "<leader>d", '\"_d')
