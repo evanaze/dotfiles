f0455b6c266e2db89b94e9ec8f1b2963e978bfa5
Evan Azevedo <me@evanazevedo.com>
2024-02-27 15:35:26 -0700

    vimspector

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-        local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            ensure_installed = { "python", "kotlin", "markdown", "bash", "lua", "vim", "html" },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },  
-        })
-    end
+		configs.setup({
+			ensure_installed = { "python", "kotlin", "markdown", "bash", "lua", "vim", "html" },
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
