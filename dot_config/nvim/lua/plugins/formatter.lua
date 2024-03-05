02a19d69799101cc7ac711ff7f31101f2d338ae1
Evan Azevedo <me@evanazevedo.com>
2024-03-05 10:36:38 -0700

    main remove autoformatting

--- dot_config/nvim/lua/plugins/formatter.lua
+++ dot_config/nvim/lua/plugins/formatter.lua
@@ -1,10 +1,12 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {
-		ensure_installed = {},
-		formatters = {
-			shfmt = {
-				prepend_args = { "-i", "4" },
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			ensure_installed = {},
+			formatters = {
+				shfmt = {
+					prepend_args = { "-i", "4" },
+				},
 			},
 		},
 	},
